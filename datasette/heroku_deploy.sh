a2f1dfb780a1c1b558e9cb5bc6259b8780de9a7d
Sergio Sanchez <ssanchez@alluma.org>
2020-05-03 16:51:37 -0700

    add template dir to command

--- datasette/heroku_deploy.sh
+++ datasette/heroku_deploy.sh
@@ -1,1 +1,7 @@
-datasette publish heroku datasette/ripa-2018-db.db --name ripa-2018-db --install datasette-vega -m datasette/updated_metadata.json --static static:datasette/static/ --extra-options="--config default_page_size:50 --config sql_time_limit_ms:90000 --config facet_time_limit_ms:3000"
+datasette publish heroku datasette/ripa-2018-db.db \
+    --name ripa-2018-db \
+    --install datasette-vega \
+    -m datasette/updated_metadata.json \
+    --static static:datasette/static/ \
+    --template-dir datasette/templates \
+    --extra-options="--config default_page_size:50 --config sql_time_limit_ms:90000 --config facet_time_limit_ms:3000"
